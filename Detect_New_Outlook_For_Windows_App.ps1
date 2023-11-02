d1e54d987b6e0a6c22ecb3d0ae282ad0dadd9640
IntuneAdmin <93092915+IntuneAdmin@users.noreply.github.com>
2023-11-02 11:11:27 +0100

    Add files via upload

---
+++ Detect_New_Outlook_For_Windows_App.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = "Microsoft.OutlookForWindows"
+
+# Check if the Outlook app is installed
+$package = Get-AppxPackage | Where-Object { $_.Name -eq $packageName }
+
+if ($package -ne $null) {
+    Write-Output "Microsoft Outlook for Windows is installed."
+    exit 1 # Return 0 to indicate a successful detection
+} else {
+    Write-Output "Microsoft Outlook for Windows is not installed."
+    exit 0 # Return 1 to indicate detection failure
+}
---
+++ Remediate_New_Outlook_For_Windows_App.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = "Microsoft.OutlookForWindows"
+
+# Check if the Outlook app is installed
+$package = Get-AppxPackage | Where-Object { $_.Name -eq $packageName }
+
+if ($package -ne $null) {
+    # Outlook is installed, so remove it
+    Write-Output "Microsoft Outlook for Windows is installed. Removing..."
+    Get-AppxPackage $packageName | Remove-AppxPackage
+    if ($?) {
+        Write-Output "Microsoft Outlook for Windows has been successfully removed."
+        exit 0 # Return 0 to indicate a successful remediation
+    } else {
+        Write-Output "Failed to remove Microsoft Outlook for Windows."
+        exit 1 # Return 1 to indicate remediation failure
+    }
+} else {
+    Write-Output "Microsoft Outlook for Windows is not installed."
+    exit 0 # Return 0 to indicate no remediation needed
+}
